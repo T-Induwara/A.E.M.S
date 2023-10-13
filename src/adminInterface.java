c52bcc0bafc9eb1e633684fe3dde46242de1f338
t-induwara <tharusha.induwar@gmail.com>
2023-10-13 18:47:53 +0530

    Customized second admin UI

--- src/adminInterface.java
+++ src/adminInterface.java
@@ -16,14 +16,64 @@ public class adminInterface {
     private JLabel appDesc;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("adminInterface");
+        JFrame frame = new JFrame("A E M S - Advance Employee Management System");
         frame.setContentPane(new adminInterface().Main);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+        // Set the taskbar icon
+        ImageIcon imgIcon = new ImageIcon("src/assets/logo/logo.png");
+        Image img = imgIcon.getImage();
+        Image newimg = img.getScaledInstance(32, 32,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        imgIcon = new ImageIcon(newimg);  // transform it back
+        frame.setIconImage(imgIcon.getImage());
     }
 
     public adminInterface() {
+        ImageIcon logoIcon = new ImageIcon("src/assets/logo/logo.png");
+        Image image = logoIcon.getImage(); // transform it
+        Image newimg = image.getScaledInstance(80, 80,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        logoIcon = new ImageIcon(newimg);  // transform it back
+        appLogo.setIcon(logoIcon);
+
+        //Font linking for Application title
+        try {
+            Font NicoMoji = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/fonts/NicoMoji-Regular.ttf")).deriveFont(80f);
+            GraphicsEnvironment font1 = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            font1.registerFont(NicoMoji);
+            appTitle.setFont(NicoMoji);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+
+        //Font linking for Application description
+        try {
+            Font Roboto = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/fonts/Roboto-Regular.ttf")).deriveFont(20f);
+            GraphicsEnvironment font2 = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            font2.registerFont(Roboto);
+            appDesc.setFont(Roboto);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+
+        //Font linking for Application btns
+        try {
+            Font RobotoBlack = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/fonts/Roboto-Black.ttf")).deriveFont(20f);
+            GraphicsEnvironment font3 = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            font3.registerFont(RobotoBlack);
+            superBtn.setFont(RobotoBlack);
+            hrBtn.setFont(RobotoBlack);
+            sysadBtn.setFont(RobotoBlack);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+
+        // Set the size of adminBtn and empBtn
+        superBtn.setPreferredSize(new Dimension(200, 40));
+        hrBtn.setPreferredSize(new Dimension(200, 40));
+        sysadBtn.setPreferredSize(new Dimension(200, 40));
+
         superBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
