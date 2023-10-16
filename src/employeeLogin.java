99c8c30a6f6a8a858dbb09f94854b29bfd6f4857
t-induwara <tharusha.induwar@gmail.com>
2023-10-16 16:22:46 +0530

    Added mysql connector and xml file to the file directory

--- src/employeeLogin.java
+++ src/employeeLogin.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import java.io.File;
+import javax.swing.JFrame;
 
 public class employeeLogin {
     private JPanel Main;
@@ -8,20 +13,63 @@ public class employeeLogin {
     private JLabel appDesc;
     private JTextField employeeEmailTextField;
     private JTextField passwordTextField;
-    private JButton loginButton;
-    private JButton button1;
-    private JButton returnButton;
+    private JButton loginBtn;
+    private JButton returnBtn;
+    private JLabel appLogo;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("employeeLogin");
+        JFrame frame = new JFrame("A E M S - Advance Employee Management System");
         frame.setContentPane(new employeeLogin().Main);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+        // Set the taskbar icon
+        ImageIcon imgIcon = new ImageIcon("src/assets/logo/logo.png");
+        Image img = imgIcon.getImage();
+        Image newimg = img.getScaledInstance(32, 32,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        imgIcon = new ImageIcon(newimg);  // transform it back
+        frame.setIconImage(imgIcon.getImage());
     }
 
     public employeeLogin() {
-        loginButton.addActionListener(new ActionListener() {
+        ImageIcon logoIcon = new ImageIcon("src/assets/logo/logo.png");
+        Image image = logoIcon.getImage(); // transform it
+        Image newimg = image.getScaledInstance(80, 80,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        logoIcon = new ImageIcon(newimg);  // transform it back
+        appLogo.setIcon(logoIcon);
+
+        //Font linking for Application title
+        try {
+            Font NicoMoji = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/fonts/NicoMoji-Regular.ttf")).deriveFont(80f);
+            GraphicsEnvironment font1 = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            font1.registerFont(NicoMoji);
+            appTitle.setFont(NicoMoji);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+
+        //Font linking for Application description
+        try {
+            Font Roboto = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/fonts/Roboto-Regular.ttf")).deriveFont(20f);
+            GraphicsEnvironment font2 = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            font2.registerFont(Roboto);
+            appDesc.setFont(Roboto);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+
+        //Font linking for Application btns
+        try {
+            Font RobotoBlack = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/fonts/Roboto-Black.ttf")).deriveFont(20f);
+            GraphicsEnvironment font3 = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            font3.registerFont(RobotoBlack);
+            loginBtn.setFont(RobotoBlack);
+            returnBtn.setFont(RobotoBlack);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+        loginBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
