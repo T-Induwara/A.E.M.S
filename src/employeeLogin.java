af5e5c073c2e47dd90371c0175c855d00de73326
Tharusha Induwara <tharusha.induwar@gmail.com>
2023-10-26 19:06:54 +0530

    Made JPanel private in employeeLogin

--- src/employeeLogin.java
+++ src/employeeLogin.java
@@ -15,7 +15,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class employeeLogin {
-    JPanel Main;
+    private JPanel Main;
     private JLabel appTitle;
     private JLabel appDesc;
     private JTextField empUsername;
