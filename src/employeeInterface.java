ccac42d2dfd466e9f3a1d326f8d457c6d9ac4fed
Tharusha Induwara <tharusha.induwar@gmail.com>
2023-10-26 21:57:07 +0530

    Added extends to JFrame in employeeInterface

--- src/employeeInterface.java
+++ src/employeeInterface.java
@@ -14,7 +14,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class employeeInterface {
+public class employeeInterface extends JFrame {
     private JPanel Main;
     private JLabel appLogo;
     private JButton viewEmpBtn;
