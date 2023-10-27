dd1efc24fc95ab5165363f255d9fd3a8df7cd16f
Tharusha Induwara <tharusha.induwar@gmail.com>
2023-10-27 19:56:49 +0530

    Added login validation codes to the hrLogin UI

--- src/hrLogin.java
+++ src/hrLogin.java
@@ -8,26 +8,32 @@ import java.io.File;
 import javax.swing.JFrame;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class hrLogin {
     private JPanel Main;
     private JLabel appLogo;
     private JLabel appTitle;
     private JTextField hrUsername;
-    private JTextField hrPass;
+    private JPasswordField hrPass;
     private JButton loginBtn;
     private JButton returnBtn;
     private JLabel appDesc;
+    private Connection con;
+    private PreparedStatement pst;
 
     public JPanel getMainPanel() {
         return Main;
     }
 
     public hrLogin() {
+        //DB Connection codes
+        DBCredentials dbCons = new DBCredentials();//Creating DB Class object
+        dbCons.connect();//Calling connection method in DBCredentials class
+
+        con = DBCredentials.getConnection();//Get con object from the DB Credentials class
+        pst = DBCredentials.getPreparedStatement();//Get pst object from the DB Credentials class
+
         ImageIcon logoIcon = new ImageIcon("src/assets/logo/logo.png");
         Image image = logoIcon.getImage(); // transform it
         Image newimg = image.getScaledInstance(80, 80,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
@@ -71,7 +77,54 @@ public class hrLogin {
         loginBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String hrEmail,hrPassLocal;
 
+                try {
+                    hrEmail = hrUsername.getText();
+                    //Convert String array into String variable
+                    char[] hrPassword = hrPass.getPassword();
+                    hrPassLocal = new String(hrPassword);
+
+                    pst = con.prepareStatement("SELECT empID,email,password FROM employee WHERE position = 'HR' AND email = ?");
+                    pst.setString(1, hrEmail);
+                    ResultSet rs = pst.executeQuery();
+
+                    if(rs.next()==true){
+                        String empID = rs.getString(1);
+                        //passEmpID = Integer.parseInt(empID);
+                        //check code
+                        System.out.println("My ID in login page is "+empID);
+                        String empEmail = rs.getString(2);
+                        String empPass = rs.getString(3);
+                        System.out.println("Pass is "+empPass);
+                        System.out.println("Entered Pass is "+empPass);
+
+                        if(hrPassLocal.equals(empPass)){//Compare user given password and the password in the db
+                            //To hide current JPanel
+                            Main.setVisible(false);
+
+                            hrInterface hrInterface = new hrInterface();
+                            //Assign JPanel of adminInterface.java to the adminMainPanel object
+                            JPanel hrPanel = hrInterface.getMainPanel();
+                            //Passing empID from login page to employee dashboard
+
+                            mainInterface.frame.setContentPane(hrPanel);
+                            mainInterface.frame.validate();
+                            mainInterface.frame.repaint();
+                            //Pass the employee ID to employee interface from the login interface
+                            //employeeInterface.setEmpID(passEmpID);
+                        }
+                        else{
+                            JOptionPane.showMessageDialog(null,"Password is incorrect!");
+                        }
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null,"Please check your email again!");
+                    }
+                }
+                catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
         returnBtn.addActionListener(new ActionListener() {
